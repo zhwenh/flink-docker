297af08de7e11f01e557e808cc21f5b5e3a79915
Andrey Melentyev <andrey.melentyev@klarna.com>
2016-11-12 18:37:36 +0100

    Allow overriding Job Manager hostname

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,16 +18,22 @@
 # limitations under the License.
 ################################################################################
 
+JOBMANAGER_HOSTNAME="$2"
+
+if [ -z "$JOBMANAGER_HOSTNAME" ]; then
+    # make use of Docker container linking and exploit the jobmanager entry in /etc/hosts
+    JOBMANAGER_HOSTNAME="flink-jobmanager"
+fi 
+
 if [ "$1" = "jobmanager" ]; then
-    # make use of container linking and exploit the jobmanager entry in /etc/hosts
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: flink-master/g" $FLINK_HOME/conf/flink-conf.yaml
+    
+    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: $JOBMANAGER_HOSTNAME/g" $FLINK_HOME/conf/flink-conf.yaml
 
     echo "Starting Job Manager"
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
     $FLINK_HOME/bin/jobmanager.sh start cluster
 elif [ "$1" = "taskmanager" ]; then
-    # make use of container linking and exploit the jobmanager entry in /etc/hosts
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: flink-master/g" $FLINK_HOME/conf/flink-conf.yaml
+    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: $JOBMANAGER_HOSTNAME/g" $FLINK_HOME/conf/flink-conf.yaml
 
     sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: `grep -c ^processor /proc/cpuinfo`/g" $FLINK_HOME/conf/flink-conf.yaml
 
